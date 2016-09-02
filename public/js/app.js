f90b6c42fbcdc6147ae62252094de0ed8a3732aa
jooordan <jespejoo@yahoo.com>
2016-09-02 15:07:09 -0700

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ console.log(name + ' wants to join ' + room);
 jQuery('.room-title').text(room);
 
 
-//when someone connects
+//when someone connects io succesuuyl request to join room
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
 	socket.emit('joinRoom', {
@@ -19,12 +19,16 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
-	var $message = jQuery('.messages');
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>');
+
+
 	console.log('new message');
 	console.log(message.text);
 
 	$message.append('<p><strong>'+ message.name + ' ' + momentTimestamp.local().format('h:mm:ss a') +'</strong></p>');
 	$message.append('<p>'+ message.text + '</p>');
+	$messages.append($message);
 });
 
 // Handles submitting of new message
